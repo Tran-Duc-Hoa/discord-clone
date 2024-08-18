a2553cbfc6695847fbc75a9e37627c1f9e10d89f
Tran-Duc-Hoa <tranduchoa611@gmail.com>
2024-08-18 14:58:14 +0700

    Make some improvements

--- lib/db.ts
+++ lib/db.ts
@@ -4,6 +4,6 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const db = new PrismaClient();
+export const db = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = db;
